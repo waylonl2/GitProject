b85bcf270673d35639f3e8427db54aec5596adbc
BT_1N3 <sample@bths.edu>
2025-11-23 00:07:48 -0500

    added some variables, did some math, and printed the answer

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,9 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("super cool program");
+        int num1 = 9;
+        int num2 = 10;
+        int sum =  num1 + num2;
+        System.out.println(sum);
     }
 }
