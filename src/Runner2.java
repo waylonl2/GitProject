93d5340e90bd9dfbe59ea9123f52215e7c5fe18b
BT_1N3 <sample@bths.edu>
2025-11-23 01:00:16 -0500

    Added new variable to Runner2 class and some print statements

--- src/Runner2.java
+++ src/Runner2.java
@@ -2,5 +2,8 @@ public class Runner2 {
     public static void main(String[] args) {
         String pokemon1 = "pikachu";
         System.out.println(pokemon1);
+        String pokemon2 = "eevee";
+        System.out.println(pokemon2);
+        System.out.println("look! a " + pokemon1 + " and a " + pokemon2);
     }
 }
