bf3f721c57e28d58798449734a7a4ffd021cc7f3
WaylonLi <sample@bths.edu>
2025-11-24 09:46:57 -0500

    did 11/24 do now with new runner class

---
+++ src/Runner3.java
@@ -0,0 +1,9 @@
+public class Runner3 {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter >= 0) {
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+    }
+}
